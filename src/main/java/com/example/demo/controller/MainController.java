ad38564fc26bd571be750439cb0d3d910ad8d807
zxw734357567 <734357567@qq.com>
2019-03-28 23:07:48 +0800

    构造项目

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2019-3-28.
+ */
+@RestController
+public class MainController {
+    @RequestMapping(value = "/sd")
+    public String main(){
+        return "你进来了!";
+    }
+
+}
