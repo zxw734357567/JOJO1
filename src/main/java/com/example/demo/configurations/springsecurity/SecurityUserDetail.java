9c9f7a0922f8569166f8879ea95ba0658d902a18
zxw734357567 <734357567@qq.com>
2019-04-10 22:00:08 +0800

    1.mybatis插件自动生成实体,mapper文件 2.使用springbootJPA

--- src/main/java/com/example/demo/configurations/springsecurity/SecurityUserDetail.java
+++ src/main/java/com/example/demo/configurations/springsecurity/SecurityUserDetail.java
@@ -1,4 +1,4 @@
-package com.example.demo.configurations;
+package com.example.demo.configurations.springsecurity;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
